fa584a54e6a1c19546bdc132b08db67f52165370
Arseniy Zakharov <a.zaharov@aristos.pw>
2022-04-17 18:03:32 +0300

    Refactored: popup component

---
+++ src/components/ui/uiPopupInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label ref="english" class="modal__edit-label" for="modal__edit-noun">{{ part }}</label>
+    <input class="modal__edit-input" :data-part="part" ref="trans_input" type="text"
+           autocomplete="off" id='modal__edit-noun' :value="word" @input="handler"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['word', 'part', 'handler', 'isActive'],
+  watch: {
+    isActive: function (val) {
+      if (val === true) {
+        this.$nextTick(() => {
+          setTimeout(() => {
+            this.$refs.english.focus()
+          }, 500)
+        }, 300)
+      }
+    }
+  }
+}
+</script>
+
